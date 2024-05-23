c7bc0f831d70f7111e84c2777fc937b2abb2185c
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-23 10:14:32 +0500

    -Firebase Auth Setup (#20)

--- composeApp/src/commonMain/kotlin/org/mind/app/domain/repository/Repository.kt
+++ composeApp/src/commonMain/kotlin/org/mind/app/domain/repository/Repository.kt
@@ -1,18 +1,13 @@
 package org.mind.app.domain.repository
 
 import dev.gitlive.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import org.mind.app.data.repository.AuthService
 import org.mind.app.domain.model.user.User
 
 class Repository(
-    val auth: FirebaseAuth,
-    val scope: CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Default),
+    private val auth: FirebaseAuth,
 ) : AuthService {
     override val currentUserId: String
         get() = auth.currentUser?.uid.toString()
@@ -22,27 +17,17 @@ class Repository(
 
     override val currentUser: Flow<User> =
         auth.authStateChanged.map { it?.let { User(it.uid, it.isAnonymous) } ?: User() }
-    private suspend fun launchWithAwait(block : suspend  () -> Unit) {
-        scope.async {
-            block()
-        }.await()
-    }
-
 
     override suspend fun authenticate(email: String, password: String) {
-        scope.async {
-            auth.signInWithEmailAndPassword(email, password)
-        }
+        auth.signInWithEmailAndPassword(email, password)
     }
 
     override suspend fun createUser(email: String, password: String) {
-        launchWithAwait {
-            auth.createUserWithEmailAndPassword(email, password)
-        }
+        auth.createUserWithEmailAndPassword(email, password)
     }
 
     override suspend fun signOut() {
-        if (auth.currentUser?.isAnonymous==true){
+        if (auth.currentUser?.isAnonymous == true) {
             auth.currentUser?.delete()
         }
     }
--- composeApp/src/commonMain/kotlin/org/mind/app/presentation/viewmodel/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/mind/app/presentation/viewmodel/MainViewModel.kt
@@ -1,6 +1,46 @@
 package org.mind.app.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import org.mind.app.domain.model.user.User
 import org.mind.app.domain.repository.Repository
+import org.mind.app.domain.usecases.ResultState
 
-class MainViewModel(private val repository: Repository) : ViewModel()
+class MainViewModel(private val repository: Repository) : ViewModel() {
+    private val _loginUser = MutableStateFlow<ResultState<String>>(ResultState.Loading)
+    val loginUser = _loginUser.asStateFlow()
+
+    private val _createUser = MutableStateFlow<ResultState<String>>(ResultState.Loading)
+    val createUser = _createUser.asStateFlow()
+
+    private val _currentUser = MutableStateFlow<User?>(null)
+    val currentUser = _currentUser.asStateFlow()
+
+    fun login(email: String, password: String) {
+        viewModelScope.launch {
+            _loginUser.value = ResultState.Loading
+            try {
+                repository.authenticate(email, password)
+                _loginUser.value = ResultState.Success("Success")
+            } catch (e: Exception) {
+                _loginUser.value = ResultState.Error(e.message.toString())
+            }
+        }
+    }
+
+    fun createAccount(email: String, password: String) {
+        viewModelScope.launch {
+            _loginUser.value = ResultState.Loading
+            try {
+                repository.createUser(email, password)
+                _createUser.value = ResultState.Success("Success")
+            } catch (e: Exception) {
+                _createUser.value = ResultState.Success(e.message.toString())
+            }
+        }
+    }
+
+}
