efe9bd3b78a54fe4636eb18bbef02e684e0f01ce
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-24 19:09:31 +0500

    -Instructor Setup (#49)

--- composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/tabs/Instructors/InstructorsTab.kt
+++ composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/tabs/Instructors/InstructorsTab.kt
@@ -8,25 +8,40 @@ import androidx.compose.material.icons.filled.IntegrationInstructions
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
+import com.example.cmppreference.LocalPreference
+import com.example.cmppreference.LocalPreferenceProvider
 
 object InstructorsTab : Tab {
     @Composable
     override fun Content() {
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text("Instructors Tab")
+        LocalPreferenceProvider {
+            val preference = LocalPreference.current
+            var email by remember { mutableStateOf("") }
+            var isLogin by remember { mutableStateOf(false) }
+            LaunchedEffect(Unit){
+                email = preference.getString("email").toString()
+                isLogin = preference.getBoolean("is_login",false)
+
+            }
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text("Instructors Tab $email & $isLogin")
+            }
         }
+
     }
 
     override val options: TabOptions
