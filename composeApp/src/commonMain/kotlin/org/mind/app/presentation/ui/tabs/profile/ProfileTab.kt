f2096bf8bc535bf29503e07e8d179e66a89fcd1c
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-24 10:25:55 +0500

    -Person Tab Setup (#37)

--- composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/tabs/profile/ProfileTab.kt
+++ composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/tabs/profile/ProfileTab.kt
@@ -1,12 +1,18 @@
 package org.mind.app.presentation.ui.tabs.profile
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.tab.Tab
@@ -16,7 +22,13 @@ import org.mind.app.presentation.ui.screens.home.HomeScreen
 object ProfileTab: Tab {
     @Composable
     override fun Content() {
-        Navigator(HomeScreen())
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text("Profile Tab")
+        }
     }
 
     override val options: TabOptions
