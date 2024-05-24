d32f94bd87fb7cf6e9c4dc52d83f3ab2e6e00269
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-24 13:05:27 +0500

    -Reset Password Impl (#40)

--- composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/auth/reset/ResetPasswordScreen.kt
+++ composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/auth/reset/ResetPasswordScreen.kt
@@ -6,11 +6,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,59 +35,76 @@ import org.koin.compose.koinInject
 import org.mind.app.presentation.viewmodel.MainViewModel
 import org.mind.app.utils.isValidEmail
 
-class ResetPasswordScreen: Screen {
+class ResetPasswordScreen : Screen {
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     override fun Content() {
         var email by remember { mutableStateOf("") }
         var userMessage by remember { mutableStateOf("") }
         val viewModel: MainViewModel = koinInject()
         val navigator = LocalNavigator.current
-
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "Reset Password",
-                style = MaterialTheme.typography.headlineLarge,
-                modifier = Modifier.padding(bottom = 32.dp)
-            )
-
-            OutlinedTextField(
-                value = email,
-                onValueChange = { email = it },
-                label = { Text("Email") },
-                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp)
-            )
-
-            Button(
-                onClick = {
-                    if (!isValidEmail(email)) {
-                        userMessage = "Invalid email format"
-                    } else {
-                      //  viewModel.resetPassword(email)
-                        userMessage = "Password reset link sent to your email."
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("") },
+                    navigationIcon = {
+                        IconButton(onClick = { navigator?.pop() }) {
+                            Icon(
+                                imageVector = Icons.Filled.ArrowBackIosNew,
+                                contentDescription = "Back"
+                            )
+                        }
                     }
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(bottom = 16.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
-            ) {
-                Text(text = "Reset Password", fontSize = 16.sp, color = Color.White)
-            }
-
-            if (userMessage.isNotEmpty()) {
-                Text(
-                    userMessage,
-                    color = MaterialTheme.colorScheme.primary
                 )
+
+            }
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Reset Password",
+                    style = MaterialTheme.typography.headlineLarge,
+                    modifier = Modifier.padding(bottom = 32.dp)
+                )
+
+                OutlinedTextField(
+                    value = email,
+                    onValueChange = { email = it },
+                    label = { Text("Email") },
+                    keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp)
+                )
+
+                Button(
+                    onClick = {
+                        if (!isValidEmail(email)) {
+                            userMessage = "Invalid email format"
+                        } else {
+                            //  viewModel.resetPassword(email)
+                            userMessage = "Password reset link sent to your email."
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
+                ) {
+                    Text(text = "Reset Password", fontSize = 16.sp, color = Color.White)
+                }
+
+                if (userMessage.isNotEmpty()) {
+                    Text(
+                        userMessage,
+                        color = MaterialTheme.colorScheme.primary
+                    )
+                }
             }
         }
     }
