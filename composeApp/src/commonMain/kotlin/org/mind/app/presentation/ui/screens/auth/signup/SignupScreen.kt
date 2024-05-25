922d6bff18baa9a094414d16fa3f9d279d991714
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-25 17:23:44 +0500

    -User Auth Completion Impl (#59)

--- composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/auth/signup/SignupScreen.kt
+++ composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/auth/signup/SignupScreen.kt
@@ -36,6 +36,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -161,8 +162,7 @@ fun SignupContent(viewModel: MainViewModel = koinInject()) {
 
             is ResultState.Success -> {
                 val response = (serverState as ResultState.Success).data
-               // userMessage = response
-                navigator?.push(LoginScreen)
+                //userMessage = response
                 isLoading = false
             }
         }
