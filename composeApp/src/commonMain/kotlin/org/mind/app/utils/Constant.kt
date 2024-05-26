a559bd2a2b7194ba5f0ffcaa5e55f19ecdce097e
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-26 12:53:42 +0500

    -User Profile init (#62)

--- composeApp/src/commonMain/kotlin/org/mind/app/utils/Constant.kt
+++ composeApp/src/commonMain/kotlin/org/mind/app/utils/Constant.kt
@@ -1,6 +1,6 @@
 package org.mind.app.utils
 
 object Constant {
-    const val BASE_URL = "http://192.168.10.5:8080/"
+    const val BASE_URL = "http://192.168.10.2:8080/"
     const val WebClientId = "315093871386-cmkrl02cictqj5ea4bbdpe3a48j3ulba.apps.googleusercontent.com"
 }
