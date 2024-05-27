c4f862680d6940b9bf990f08d17bf013429e23f4
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-27 08:27:33 +0500

    -Chat Bot Screen Impl (#83)

---
+++ composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/chatbot/ChatBotScreen.kt
@@ -0,0 +1,23 @@
+package org.mind.app.presentation.ui.screens.chatbot
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.core.screen.Screen
+
+class ChatBotScreen: Screen {
+    @Composable
+    override fun Content() {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Text("Chat Bot")
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/tabs/chat/ChatTab.kt
+++ composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/tabs/chat/ChatTab.kt
@@ -1,31 +1,23 @@
 package org.mind.app.presentation.ui.tabs.chat
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ChatBubble
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
+import org.mind.app.presentation.ui.screens.chatbot.ChatBotScreen
 
 object ChatTab : Tab {
     @Composable
     override fun Content() {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Text("Chat Bot")
-        }
+        Navigator(ChatBotScreen())
     }
 
     override val options: TabOptions
         @Composable
-        get(){
+        get() {
             val icon = rememberVectorPainter(Icons.Default.ChatBubble)
             val title = "Chat Bot"
             val index: UShort = 3u
