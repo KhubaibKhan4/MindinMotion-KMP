5e38ed2b9abacb3d24572852ed9bd08df4159ee2
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-28 20:42:58 +0500

    -Super Quiz Screen Impl (#110)

--- composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/quiz/QuizScreenPlay.kt
+++ composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/quiz/QuizScreenPlay.kt
@@ -1,5 +1,6 @@
 package org.mind.app.presentation.ui.screens.quiz
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -16,7 +17,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AttachMoney
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -27,13 +28,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
-import cafe.adriel.voyager.navigator.tab.TabNavigator
+import mind_in_motion.composeapp.generated.resources.Res
+import mind_in_motion.composeapp.generated.resources.bitcoins
+import org.jetbrains.compose.resources.painterResource
 import org.mind.app.domain.model.category.QuizCategoryItem
 import org.mind.app.domain.model.quiz.QuizQuestionsItem
 import org.mind.app.presentation.ui.tabs.quiz.QuizQuestions
@@ -64,84 +73,123 @@ fun QuizScreenPlayContent(
                         Color(0xFF8A2BE2)
                     )
                 )
-            )
-            .padding(16.dp),
+            ),
         contentAlignment = Alignment.Center
     ) {
         Column(
             modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.Top
         ) {
-
+            Spacer(modifier = Modifier.height(100.dp))
             Row(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.Star,
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp),
+                            tint = Color.Yellow
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Icon(
+                            imageVector = Icons.Default.Star,
+                            contentDescription = null,
+                            modifier = Modifier.size(24.dp),
+                            tint = Color.Yellow
+                        )
+                        Spacer(modifier = Modifier.width(4.dp))
+                        Icon(
+                            imageVector = Icons.Default.Star,
+                            contentDescription = null,
+                            modifier = Modifier.size(16.dp),
+                            tint = Color.Yellow
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .size(60.dp)
+                            .clip(CircleShape)
+                            .background(Color.Yellow)
+                    ) {
+                        Text(
+                            text = "200",
+                            color = Color.Black,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
                 Row(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Icon(
-                        imageVector = Icons.Default.AttachMoney,
+                    Image(
+                        painter = painterResource(Res.drawable.bitcoins),
                         contentDescription = null,
-                        modifier = Modifier.size(24.dp)
-                    )
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Icon(
-                        imageVector = Icons.Default.AttachMoney,
-                        contentDescription = null,
-                        modifier = Modifier.size(24.dp)
-                    )
-                }
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier
-                        .size(60.dp)
-                        .clip(CircleShape)
-                        .background(Color.Yellow)
-                ) {
-                    Text(
-                        text = "200",
-                        color = Color.Black,
-                        fontWeight = FontWeight.Bold
+                        modifier = Modifier
+                            .size(240.dp)
+                            .rotate(80f)
+                            .weight(1f)
                     )
                 }
             }
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            Text(
-                text = "Sunday's",
-                style = MaterialTheme.typography.headlineLarge,
-                fontWeight = FontWeight.Bold,
-                color = Color.White
-            )
-            Text(
-                text = "Super Quiz",
-                style = MaterialTheme.typography.headlineMedium,
-                fontWeight = FontWeight.Bold,
-                color = Color.White
-            )
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(
-                text = "Play Super Quiz & earn ",
-                style = MaterialTheme.typography.bodyMedium,
-                color = Color.White
-            )
-            Text(
-                text = "200 Coin",
-                style = MaterialTheme.typography.bodyMedium,
-                fontWeight = FontWeight.Bold,
-                color = Color.White
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "Sunday's",
+                    style = MaterialTheme.typography.headlineLarge,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    textAlign = TextAlign.Start,
+                    fontSize = 38.sp
+                )
+                Text(
+                    text = "Super Quiz",
+                    style = MaterialTheme.typography.headlineMedium,
+                    fontWeight = FontWeight.ExtraBold,
+                    color = Color.White,
+                    textAlign = TextAlign.Start,
+                    fontSize = 55.sp
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    buildAnnotatedString {
+                        append("Play Super Quiz & earn ")
+                        withStyle(style = SpanStyle(fontWeight = FontWeight.Bold)) {
+                            append("200")
+                        }
+                        append(" Coins")
+                    },
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = Color.White,
+                    textAlign = TextAlign.Start
+                )
+            }
 
             Spacer(modifier = Modifier.height(16.dp))
 
             Card(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp),
+                    .fillMaxWidth(),
                 shape = RoundedCornerShape(topStart = 16.dp, topEnd = 16.dp),
                 elevation = CardDefaults.cardElevation(8.dp)
             ) {
@@ -160,7 +208,14 @@ fun QuizScreenPlayContent(
                     Text(
                         text = quizCategoryItem.name,
                         style = MaterialTheme.typography.headlineSmall,
-                        color = Color.Blue
+                        color = Color.Blue,
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 38.sp
+                    )
+                    Text(
+                        text = quizCategoryItem.description,
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = Color.Black
                     )
                     Spacer(modifier = Modifier.height(16.dp))
                     Box(
@@ -177,7 +232,8 @@ fun QuizScreenPlayContent(
                                 )
                             )
                             .clickable {
-                                       navigator.current = QuizQuestions(quizCategoryItem, quizQuestionsItem)
+                                navigator.current =
+                                    QuizQuestions(quizCategoryItem, quizQuestionsItem)
                             },
                         contentAlignment = Alignment.Center
                     ) {
@@ -187,6 +243,13 @@ fun QuizScreenPlayContent(
                             fontWeight = FontWeight.Bold
                         )
                     }
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(
+                        text = "${quizQuestionsItem.size} Questions",
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = Color.Black
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
                 }
             }
         }
