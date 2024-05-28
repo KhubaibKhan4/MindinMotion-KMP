b256b2f6151a045c359f5c42194bfae66190d3e8
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-28 15:16:55 +0500

    -Quiz Screen Enhancement Impl (#102)

--- composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/quiz/QuizScreen.kt
+++ composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/quiz/QuizScreen.kt
@@ -1,5 +1,6 @@
 package org.mind.app.presentation.ui.screens.quiz
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -7,21 +8,19 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Scaffold
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -38,11 +37,9 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
-import cafe.adriel.voyager.navigator.tab.TabNavigator
 import io.kamel.core.Resource
 import io.kamel.image.KamelImage
 import io.kamel.image.asyncPainterResource
-import mind_in_motion.composeapp.generated.resources.Res
 import org.koin.compose.koinInject
 import org.mind.app.domain.model.category.QuizCategoryItem
 import org.mind.app.domain.model.quiz.QuizQuestionsItem
@@ -50,7 +47,9 @@ import org.mind.app.domain.usecases.ResultState
 import org.mind.app.presentation.ui.components.ErrorBox
 import org.mind.app.presentation.ui.components.LoadingBox
 import org.mind.app.presentation.ui.tabs.quiz.QuizQuestions
+import org.mind.app.presentation.ui.tabs.quiz.QuizTab
 import org.mind.app.presentation.viewmodel.MainViewModel
+import org.mind.app.theme.LocalThemeIsDark
 import org.mind.app.utils.Constant.BASE_URL
 
 class QuizScreen : Screen {
@@ -60,9 +59,10 @@ class QuizScreen : Screen {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun QuizScreenContent(
-    viewModel: MainViewModel = koinInject()
+    viewModel: MainViewModel = koinInject(),
 ) {
     var quizQuestions by remember { mutableStateOf<List<QuizQuestionsItem>?>(null) }
     var categories by remember { mutableStateOf<List<QuizCategoryItem>?>(null) }
@@ -78,9 +78,11 @@ fun QuizScreenContent(
             val error = (quizState as ResultState.Error).message
             ErrorBox(error)
         }
+
         ResultState.Loading -> {
             LoadingBox()
         }
+
         is ResultState.Success -> {
             val response = (quizState as ResultState.Success).data
             quizQuestions = response
@@ -93,9 +95,11 @@ fun QuizScreenContent(
             val error = (categoryState as ResultState.Error).message
             ErrorBox(error)
         }
+
         ResultState.Loading -> {
             LoadingBox()
         }
+
         is ResultState.Success -> {
             val response = (categoryState as ResultState.Success).data
             categories = response
@@ -111,27 +115,39 @@ fun QuizScreenContent(
         }
     }
 
-    Column(
-        modifier = Modifier.fillMaxWidth()
-            .padding(6.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("Quiz")
+                },
+            )
+        }
     ) {
-        LazyColumn(
-            modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+        Column(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.surface)
+                .fillMaxWidth()
+                .padding(top = it.calculateTopPadding(), start = 16.dp, end = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            categories?.forEach { category ->
-                val quizItemsForCategory = quizItemsWithCategories
-                    ?.filter { it.second.id == category.id }
-                    ?.map { it.first }
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                categories?.forEach { category ->
+                    val quizItemsForCategory = quizItemsWithCategories
+                        ?.filter { it.second.id == category.id }
+                        ?.map { it.first }
 
-                item {
-                    if (quizItemsForCategory != null && quizItemsForCategory.isNotEmpty()) {
-                        QuizCategoryItemCard(quizItemsForCategory, category)
-                    } else {
-                        Text("No Items Found for ${category.name}")
+                    item {
+                        if (quizItemsForCategory != null && quizItemsForCategory.isNotEmpty()) {
+                            QuizCategoryItemCard(quizItemsForCategory, category)
+                        } else {
+                            Text("No Items Found for ${category.name}")
+                        }
                     }
                 }
             }
@@ -142,31 +158,27 @@ fun QuizScreenContent(
 @Composable
 fun QuizCategoryItemCard(
     quizItems: List<QuizQuestionsItem>,
-    category: QuizCategoryItem
+    category: QuizCategoryItem,
 ) {
     val navigator = LocalTabNavigator.current
+    val isDark by LocalThemeIsDark.current
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
+            .padding(horizontal = 16.dp)
             .clickable {
-                navigator.current = QuizQuestions(category,quizItems)
+                navigator.current = QuizQuestions(category, quizItems)
             },
+        border = BorderStroke(width = if(isDark) 1.dp else 2.dp, color = MaterialTheme.colorScheme.primary),
         elevation = CardDefaults.cardElevation(8.dp),
         shape = RoundedCornerShape(16.dp)
     ) {
         Box(
-            modifier = Modifier.background(
-                brush = Brush.verticalGradient(
-                    colors = listOf(
-                        MaterialTheme.colorScheme.primary,
-                        MaterialTheme.colorScheme.secondary
-                    )
-                )
-            )
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.surface)
+                .padding(16.dp)
         ) {
             Row(
-                modifier = Modifier.padding(16.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 val image: Resource<Painter> = asyncPainterResource(BASE_URL + category.imageUrl)
@@ -184,13 +196,13 @@ fun QuizCategoryItemCard(
                     Text(
                         text = category.name,
                         style = MaterialTheme.typography.headlineSmall,
-                        color = MaterialTheme.colorScheme.onPrimary,
+                        color = MaterialTheme.colorScheme.onSurface,
                         modifier = Modifier.padding(bottom = 4.dp)
                     )
                     Text(
                         text = category.description,
                         style = MaterialTheme.typography.bodyMedium,
-                        color = MaterialTheme.colorScheme.onPrimary.copy(alpha = 0.8f)
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.8f)
                     )
                 }
             }
