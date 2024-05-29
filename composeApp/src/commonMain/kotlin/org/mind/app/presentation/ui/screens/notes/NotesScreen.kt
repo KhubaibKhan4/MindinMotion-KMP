41e5446315b5c4d67075f11233e9d8f3ebb2a6a7
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-29 11:27:20 +0500

    -Tab Row Impl (#121)

--- composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/notes/NotesScreen.kt
+++ composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/notes/NotesScreen.kt
@@ -2,9 +2,11 @@ package org.mind.app.presentation.ui.screens.notes
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -12,10 +14,16 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Article
+import androidx.compose.material.icons.automirrored.filled.EventNote
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -26,6 +34,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -50,21 +59,26 @@ class NotesScreen : Screen {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NotesScreenContent(
-    viewModel: MainViewModel= koinInject()
+    viewModel: MainViewModel = koinInject(),
 ) {
     var notesList by remember { mutableStateOf(emptyList<Notes>()) }
-    LaunchedEffect(Unit){
+    var selectedTabIndex by remember { mutableStateOf(0) }
+
+    LaunchedEffect(Unit) {
         viewModel.getAllNotes()
     }
+
     val notesState by viewModel.notes.collectAsState()
-    when(notesState){
+    when (notesState) {
         is ResultState.Error -> {
             val error = (notesState as ResultState.Error).message
             ErrorBox(error)
         }
+
         ResultState.Loading -> {
             LoadingBox()
         }
+
         is ResultState.Success -> {
             val notes = (notesState as ResultState.Success).data
             notesList = notes.sortedByDescending { it.id }
@@ -80,24 +94,72 @@ fun NotesScreenContent(
                     titleContentColor = Color.White
                 )
             )
+        },
+        content = {
+            Column(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                TabRow(
+                    selectedTabIndex = selectedTabIndex,
+                ) {
+                    Tab(
+                        selected = selectedTabIndex == 0,
+                        onClick = { selectedTabIndex = 0 },
+                        icon = {
+                            Icon(
+                                Icons.AutoMirrored.Filled.EventNote,
+                                contentDescription = "Notes"
+                            )
+                        },
+                        text = { Text("Notes", fontWeight = FontWeight.Bold) }
+                    )
+                    Tab(
+                        selected = selectedTabIndex == 1,
+                        onClick = { selectedTabIndex = 1 },
+                        icon = {
+                            Icon(
+                                Icons.AutoMirrored.Filled.Article,
+                                contentDescription = "Papers"
+                            )
+                        },
+                        text = { Text("Papers", fontWeight = FontWeight.Bold) }
+                    )
+                }
+                when (selectedTabIndex) {
+                    0 -> {
+                        LazyColumn(
+                            contentPadding = PaddingValues(16.dp),
+                            verticalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            items(notesList) { note ->
+                                NoteItem(note)
+                            }
+                        }
+                    }
+
+                    1 -> {
+
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(text = "Papers Content Goes Here")
+                        }
+                    }
+                }
+            }
         }
-    ) {
-        LazyColumn(
-            contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-           items(notesList){note ->
-               NoteItem(note)
-           }
-        }
-    }
+    )
 }
+
 @Composable
 fun NoteItem(note: Notes) {
     val navigator = LocalNavigator.current
     Card(
         shape = RoundedCornerShape(8.dp),
-        elevation =CardDefaults.cardElevation( 4.dp),
+        elevation = CardDefaults.cardElevation(4.dp),
         modifier = Modifier
             .fillMaxWidth()
             .clickable { navigator?.push(NotesViewScreen(note)) }
