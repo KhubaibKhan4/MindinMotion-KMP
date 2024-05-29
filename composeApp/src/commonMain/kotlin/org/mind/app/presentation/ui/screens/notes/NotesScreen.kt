c188d5e1b085194fe84a32485e1ee48745237b35
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-05-29 18:11:51 +0500

    -Boarding UI COmpletion (#126)

--- composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/notes/NotesScreen.kt
+++ composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/notes/NotesScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,7 +12,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -27,7 +25,6 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
@@ -47,6 +44,7 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
@@ -191,10 +189,12 @@ fun NotesScreenContent(
                             .fillMaxSize(),
                         contentAlignment = Alignment.Center
                     ) {
-                        LazyColumn(
+                        LazyVerticalGrid(
+                            columns = GridCells.Fixed(2),
                             modifier = Modifier.fillMaxWidth(),
-                            horizontalAlignment = Alignment.CenterHorizontally,
-                            verticalArrangement = Arrangement.spacedBy(8.dp)
+                            horizontalArrangement = Arrangement.spacedBy(12.dp),
+                            verticalArrangement = Arrangement.spacedBy(12.dp),
+                            contentPadding = PaddingValues(12.dp)
                         ) {
                             val filteredBoards = if (searchText.text.isEmpty()) {
                                 boardsList
@@ -260,17 +260,18 @@ fun BoardItem(
     board: Boards,
 ) {
     val navigator = LocalNavigator.current
-    Surface(
+    Card(
         modifier = Modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(8.dp),
-        tonalElevation = 4.dp
+        shape = RoundedCornerShape(8.dp)
     ) {
-        Row(
-            modifier = Modifier
+        Column(
+            modifier = Modifier.fillMaxWidth()
+                .padding(6.dp)
                 .clickable {
                     navigator?.push(BoardDetailScreen(board))
-                }
-                .padding(16.dp)
+                },
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
             val image: io.kamel.core.Resource<Painter> =
                 asyncPainterResource(BASE_URL + board.imageUrl)
@@ -282,21 +283,14 @@ fun BoardItem(
                 contentScale = ContentScale.Crop
             )
             Spacer(modifier = Modifier.width(16.dp))
-            Column {
-                Text(
-                    text = board.title,
-                    style = MaterialTheme.typography.headlineSmall,
-                    color = MaterialTheme.colorScheme.primary
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(
-                    text = board.description,
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = Color.Gray,
-                    maxLines = 3,
-                    overflow = TextOverflow.Ellipsis
-                )
-            }
+            Text(
+                text = board.title,
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.primary,
+                textAlign = TextAlign.Center,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
         }
     }
 }
