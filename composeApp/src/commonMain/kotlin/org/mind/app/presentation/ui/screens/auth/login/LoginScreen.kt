ef74ada396c6202a05bfd4acacbacd4be9676ad6
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-06-06 13:33:29 +0500

    -Logo Setup (#200)

--- composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/auth/login/LoginScreen.kt
+++ composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/auth/login/LoginScreen.kt
@@ -1,6 +1,8 @@
 package org.mind.app.presentation.ui.screens.auth.login
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,11 +10,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
@@ -36,6 +40,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.text.input.ImeAction
@@ -50,6 +55,9 @@ import com.example.cmppreference.LocalPreference
 import com.example.cmppreference.LocalPreferenceProvider
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import mind_in_motion.composeapp.generated.resources.Res
+import mind_in_motion.composeapp.generated.resources.ic_logo
+import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.koinInject
 import org.mind.app.domain.usecases.ResultState
 import org.mind.app.notify
@@ -116,15 +124,15 @@ fun LoginContent(
                 notify(userMessage)
                 isLoading = false
                 if (userMessage.contains("Success")) {
-                   scope.launch {
-                       email = ""
-                       pass = ""
-                       navigator?.apply {
-                           preference.put("is_login", true)
-                           push(MainScreen)
-                       }
-                       delay(2.seconds)
-                   }
+                    scope.launch {
+                        email = ""
+                        pass = ""
+                        navigator?.apply {
+                            preference.put("is_login", true)
+                            push(MainScreen)
+                        }
+                        delay(2.seconds)
+                    }
                 }
             }
         }
@@ -138,6 +146,14 @@ fun LoginContent(
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Image(
+                painter = painterResource(Res.drawable.ic_logo),
+                contentDescription = null,
+                modifier = Modifier.size(120.dp)
+                    .clip(RoundedCornerShape(12.dp))
+                    .border(width = 1.dp, color = if (isDark) Color.White else Color.Black, shape = RoundedCornerShape(12.dp))
+            )
+            Spacer(modifier = Modifier.height(20.dp))
             Text(
                 text = "Login",
                 style = MaterialTheme.typography.headlineLarge,
