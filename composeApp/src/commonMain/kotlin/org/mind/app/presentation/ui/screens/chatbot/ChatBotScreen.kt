c0590f3148d94dd7e8df40fa8d7c7c36e4c67226
Muhammad Khubaib Imtiaz <18.bscs.803@gmail.com>
2024-06-09 10:30:04 +0500

    -User Profile Enhancement (#225)

--- composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/chatbot/ChatBotScreen.kt
+++ composeApp/src/commonMain/kotlin/org/mind/app/presentation/ui/screens/chatbot/ChatBotScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -42,7 +43,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -50,10 +51,12 @@ import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
 import com.example.cmppreference.LocalPreference
 import com.example.cmppreference.LocalPreferenceProvider
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
 import mind_in_motion.composeapp.generated.resources.Res
 import mind_in_motion.composeapp.generated.resources.avatar
-import mind_in_motion.composeapp.generated.resources.ic_cyclone
 import mind_in_motion.composeapp.generated.resources.ic_logo
+import org.jetbrains.compose.resources.Resource
 import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.koinInject
 import org.mind.app.domain.model.message.Message
@@ -68,6 +71,7 @@ import org.mind.app.presentation.ui.components.parseMessageText
 import org.mind.app.presentation.ui.tabs.home.HomeTab
 import org.mind.app.presentation.viewmodel.MainViewModel
 import org.mind.app.theme.LocalThemeIsDark
+import org.mind.app.utils.Constant
 
 class ChatBotScreen : Screen {
     @Composable
@@ -88,25 +92,27 @@ fun ChatScreenContent(viewModel: MainViewModel = koinInject()) {
         var isInfo by remember { mutableStateOf(false) }
         val isDark by LocalThemeIsDark.current
         val navigator = LocalTabNavigator.current
-        LaunchedEffect(Unit){
+        LaunchedEffect(Unit) {
             viewModel.getUserByEmail(email.toString())
         }
         val currentUserState by viewModel.userByEmail.collectAsState()
-        when(currentUserState){
+        when (currentUserState) {
             is ResultState.Error -> {
                 val error = (currentUserState as ResultState.Error).message
                 ErrorBox(error)
             }
+
             ResultState.Loading -> {
                 LoadingBox()
             }
+
             is ResultState.Success -> {
                 val response = (currentUserState as ResultState.Success).data
                 currentUserId = response
             }
         }
-        LaunchedEffect(currentUserId){
-            if (currentUserId?.id!=null){
+        LaunchedEffect(currentUserId) {
+            if (currentUserId?.id != null) {
                 viewModel.fetchBotMessages(currentUserId = currentUserId?.id.toString())
             }
         }
@@ -154,7 +160,9 @@ fun ChatScreenContent(viewModel: MainViewModel = koinInject()) {
                             reverseLayout = true
                         ) {
                             items(messages.reversed()) { message ->
-                                MessageBubble(message)
+                                currentUserId?.let {
+                                    MessageBubble(message,it)
+                                }
                             }
                         }
                     }
@@ -178,7 +186,10 @@ fun ChatScreenContent(viewModel: MainViewModel = koinInject()) {
                             IconButton(
                                 onClick = {
                                     if (userInput.isNotBlank()) {
-                                        viewModel.sendMessage(userInput, currentUserId = currentUserId?.id.toString())
+                                        viewModel.sendMessage(
+                                            userInput,
+                                            currentUserId = currentUserId?.id.toString()
+                                        )
                                         userInput = ""
                                     }
                                 }
@@ -206,8 +217,12 @@ fun ChatScreenContent(viewModel: MainViewModel = koinInject()) {
         }
     }
 }
+
 @Composable
-fun MessageBubble(message: Message) {
+fun MessageBubble(
+    message: Message,
+    users: Users,
+) {
     val isDark by LocalThemeIsDark.current
     Row(
         modifier = Modifier
@@ -261,16 +276,43 @@ fun MessageBubble(message: Message) {
             }
         }
         if (message.isUserMessage) {
-            Image(
-                painter = painterResource(Res.drawable.avatar),
-                contentDescription = "User Profile",
-                modifier = Modifier
-                    .padding(start = 8.dp)
-                    .size(40.dp)
-                    .clip(CircleShape)
-                ,
-                contentScale = ContentScale.Crop
-            )
+            if (users.profileImage?.contains("null") == true) {
+                Box(
+                    modifier = Modifier
+                        .padding(start = 8.dp)
+                        .size(40.dp)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.surfaceContainer)
+                        .border(width = 1.dp, color = Color.Gray, shape = CircleShape)
+                ) {
+                    Text(
+                        text = users.fullName.first().toString(),
+                        modifier = Modifier.align(Alignment.Center),
+                        color = if (isDark) Color.White else Color.Black,
+                        fontSize = 24.sp
+                    )
+                }
+            } else {
+                Box(
+                    modifier = Modifier
+                        .padding(start = 8.dp)
+                        .size(40.dp)
+                        .clip(CircleShape)
+                        .background(MaterialTheme.colorScheme.surfaceContainer)
+                        .border(width = 1.dp, color = Color.Gray, shape = CircleShape)
+                ) {
+                    val image: io.kamel.core.Resource<Painter> = asyncPainterResource(Constant.BASE_URL + users.profileImage)
+                    KamelImage(
+                        resource = image,
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxSize()
+                            .clip(CircleShape)
+                            .border(width = 1.dp, color = Color.Gray, shape = CircleShape),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+
+            }
         }
     }
 }
